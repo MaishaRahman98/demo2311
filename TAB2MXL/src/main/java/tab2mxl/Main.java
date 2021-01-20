53c2b1ef8b6fc1a3e2cf013d95b4e39e5fdff166
Ali Sheikhi <alish@my.yorku.ca>
2021-01-20 12:07:39 -0500

    Tabula rasa 2

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -15,11 +15,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Tabula rasa");
-		System.out.println("Lars version");
-		System.out.println("Maisha's Version 1");
-		System.out.println("Nabaa's Version");
-		System.out.println("Ali Version");
-		System.out.println("Shawn upload");
 	}
 
 }
