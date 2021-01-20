1561affba8ea5b320a66767ad095df661ae97b92
redga <redga@192.168.0.22>
2021-01-20 11:42:38 -0500

    Lars Version

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -15,6 +15,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Tabula rasa");
+		System.out.println("Lars version");
 		
 	}
 
