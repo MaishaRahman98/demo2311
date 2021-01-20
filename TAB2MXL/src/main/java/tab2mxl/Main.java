7b9412f43f7e8f6b54b2c6b048812649bfd7b485
Ali Sheikhi <alish@my.yorku.ca>
2021-01-20 11:12:18 -0500

    Tabula rasa

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -14,21 +14,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My First Version.");
-		System.out.println("Hello testing 1,2,3!");
-		System.out.println("Hello this is shawn");
-		System.out.println("Maisha says Hi!");
-		System.out.println("alish");
-		System.out.println("Nabaa says hi");
-		System.out.println("Maisha's second hi!!!");
-		System.out.println("Alis first second");
-		System.out.println("Alis second third");
-		System.out.println("shawn second change");
-		System.out.println("Ali second change");
-		System.out.println("Maish's third change");
-		System.out.println("Lars changes");
-		System.out.println("Maish's 4th change");
-		System.out.println("Nabaa's change!  ");
+		System.out.println("Tabula rasa");
 		
 	}
 
