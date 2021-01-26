427c0e125db910923377cc4fd23e58c93b7099a7
Ali Sheikhi <alish@my.yorku.ca>
2021-01-26 18:18:17 -0500

    Instrument decision maker

--- TAB2MXL/java/tab2mxl/TablatureScanner.java
+++ TAB2MXL/java/tab2mxl/TablatureScanner.java
@@ -6,11 +6,25 @@ public class TablatureScanner {
 
   public static void main(String[] args) {
     try {
-      File myObj = new File("bassTablature.txt");
+      File myObj = new File("drumTablature.txt");
       Scanner myReader = new Scanner(myObj);
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
-        System.out.println(data);
+        if (data.startsWith("E|"))
+        {
+        	System.out.println("Guitar");
+        	break;
+        }
+        else if (data.startsWith("G|"))
+        {
+        	System.out.println("Bass");
+        	break;
+        }
+        else if (data.startsWith("C |"))
+        {
+        	System.out.println("Drums");
+        	break;
+        }
       }
       myReader.close();
     } catch (FileNotFoundException e) {
