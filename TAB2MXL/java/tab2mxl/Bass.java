76166ce659e4b2c731f1d0e8eba887cf6aea9e5b
Ali Sheikhi <alish@my.yorku.ca>
2021-01-31 21:34:47 -0500

    Bass v0.01

--- TAB2MXL/java/tab2mxl/Bass.java
+++ TAB2MXL/java/tab2mxl/Bass.java
@@ -1,15 +1,16 @@
 package tab2mxl;
 
-public class Bass {
-	char type; //number of strings
-	String s1, s2, s3, s4, s5;
-	String[] note1 = {"G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G"};
-	String[] note2 = {"D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D"};
-	String[] note3 = {"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A"};
-	String[] note4 = {"E", "F", "Gb","G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"};
-	String[] note5 = {"B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"};
+public class Bass 
+{
+	private char type; //number of strings
+	private String s1, s2, s3, s4, s5;
+	final String[] note1 = {"G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G"};
+	final String[] note2 = {"D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D"};
+	final String[] note3 = {"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A"};
+	final String[] note4 = {"E", "F", "Gb","G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"};
+	final String[] note5 = {"B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"};
 	
-	public Bass ()
+	private Bass()
 	{
 		this.type = ' ';
 		this.s1 = "";
@@ -18,7 +19,7 @@ public class Bass {
 		this.s4 = "";
 	}
 	
-	public Bass (String s1, String s2, String s3, String s4)
+	private Bass(String s1, String s2, String s3, String s4)
 	{
 		this.type = '4';
 		this.s1 = s1;
@@ -27,7 +28,7 @@ public class Bass {
 		this.s4 = s4;
 	}
 	
-	public Bass (String s1, String s2, String s3, String s4, String s5)
+	private Bass(String s1, String s2, String s3, String s4, String s5)
 	{
 		this(s1, s2, s3, s4);
 		this.s5 = s5;
@@ -45,7 +46,7 @@ public class Bass {
 	}
 	
 	public void printTab()
-	{
+	{		         
 		for (int i = 2 ; s1.charAt(i) != '|' ; i++)
 		{
 			if (Character.isDigit(s1.charAt(i)))
@@ -64,12 +65,15 @@ public class Bass {
 				System.out.println("String = 4 Fret = " + s5.charAt(i) + " Note: " + note5[Character.getNumericValue(s5.charAt(i))]);
 		}
 	}
+		
 
-	public char getType() {
+	public char getType() 
+	{
 		return type;
 	}
 
-	public void setType(char type) {
+	public void setType(char type) 
+	{
 		this.type = type;
 	}
 
@@ -77,39 +81,48 @@ public class Bass {
 		return s1;
 	}
 
-	public void setS1(String s1) {
+	public void setS1(String s1) 
+	{
 		this.s1 = s1;
 	}
 
-	public String getS2() {
+	public String getS2() 
+	{
 		return s2;
 	}
 
-	public void setS2(String s2) {
+	public void setS2(String s2) 
+	{
 		this.s2 = s2;
 	}
 
-	public String getS3() {
+	public String getS3() 
+	{
 		return s3;
 	}
 
-	public void setS3(String s3) {
+	public void setS3(String s3) 
+	{
 		this.s3 = s3;
 	}
 
-	public String getS4() {
+	public String getS4() 
+	{
 		return s4;
 	}
 
-	public void setS4(String s4) {
+	public void setS4(String s4) 
+	{
 		this.s4 = s4;
 	}
 
-	public String getS5() {
+	public String getS5() 
+	{
 		return s5;
 	}
 
-	public void setS5(String s5) {
+	public void setS5(String s5) 
+	{
 		this.s5 = s5;
 	}
 	
