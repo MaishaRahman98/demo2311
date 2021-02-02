4f995cdcf001fb2d568028829d1f7e1de1ce44d2
nabat <nabaagz@my.yorku.ca>
2021-02-02 15:28:16 -0500

    nab

--- TAB2MXL/java/tab2mxl/Main.java
+++ TAB2MXL/java/tab2mxl/Main.java
@@ -17,11 +17,10 @@ public class Main {
 		System.out.println("Tabula rasa v4");
 		System.out.println("Maisha's personal branch change");
 
-<<<<<<< HEAD
-=======
+
 		System.out.println("Nabaa");
 	
->>>>>>> refs/heads/Nabaa_Gaziy
+
 	}
 }
 
