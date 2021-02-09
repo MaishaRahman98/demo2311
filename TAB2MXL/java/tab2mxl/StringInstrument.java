cfc798c3ea16723453adf1a0bde5544007feb355
Ali Sheikhi <alish@my.yorku.ca>
2021-02-09 12:49:17 -0500

    bass debug 3

--- TAB2MXL/java/tab2mxl/StringInstrument.java
+++ TAB2MXL/java/tab2mxl/StringInstrument.java
@@ -21,26 +21,69 @@ public class StringInstrument {
 		this.str2 = str2;
 		this.str3 = str3;
 		this.str4 = str4;	
+		this.type = '4';
 	}
+	
+	public StringInstrument(String str1, String str2, String str3, String str4, String str5) {
+		this.str1 = str1;
+		this.str2 = str2;
+		this.str3 = str3;
+		this.str4 = str4;	
+		this.str5 = str4;
+		this.type = '5';
+	}
+	
 
-	public static void getBassFourString(String str1, String str2, String str3, String str4) {
+	public static Bass getBass(String str1, String str2, String str3, String str4) {
 		Bass bassFour;
 		bassFour = Bass.getInstance(str1,str2,str3,str4);
-		bassFour.printTab();
+		return bassFour;
 	}
-	public static void getBassFiveString(String str1, String str2, String str3, String str4, String str5) {
+	public static Bass getBass(String str1, String str2, String str3, String str4, String str5) {
 		Bass bassFive;
 		bassFive = Bass.getInstance(str1,str2,str3,str4,str5);
-		bassFive.printTab();
+		return bassFive;
 		
+	}
+	public String getStr1() {
+		return str1;
+	}
+	public void setStr1(String str1) {
+		this.str1 = str1;
+	}
+	public String getStr2() {
+		return str2;
+	}
+	public void setStr2(String str2) {
+		this.str2 = str2;
+	}
+	public String getStr3() {
+		return str3;
+	}
+	public void setStr3(String str3) {
+		this.str3 = str3;
+	}
+	public String getStr4() {
+		return str4;
+	}
+	public void setStr4(String str4) {
+		this.str4 = str4;
+	}
+	public String getStr5() {
+		return str5;
+	}
+	public void setStr5(String str5) {
+		this.str5 = str5;
+	}
+	public char getType() {
+		return type;
+	}
+	public void setType(char type) {
+		this.type = type;
 	}
 	
 	
-//	public static StringInstrument bassFourString(String s1, String s2, String s3, String s4) {
-//		return bassInstrument.getInstance(s1,s2,s3,s4);
-//		
-//	}
-//	public static StringInstrument bassFiveString(String s1, String s2, String s3, String s4, String s5) {
-//		return bassInstrument.getInstance(s1,s2,s3,s4,s5);
-//	}
 }
+	
+	
+
