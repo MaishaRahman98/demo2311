190e7edb21a4059c1164ceb0ef7b6b013b2818eb
MaishaRahman98 <maisha98@my.yorku.ca>
2021-02-09 12:50:21 -0500

    Added code in printTab method in Bass class to print notes in xml format

--- TAB2MXL/java/tab2mxl/Bass.java
+++ TAB2MXL/java/tab2mxl/Bass.java
@@ -38,22 +38,91 @@ public class Bass extends StringInstrument
 	
 	public void printTab()
 	{		         
+		//XML declarations:
+		System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+		System.out.println("<!DOCTYPE score-partwise PUBLIC \"-//Recordare//DTD MusicXML 3.1 Partwise//EN\" \"http://www.musicxml.org/dtds/partwise.dtd\">");
 		for (int i = 2 ; s1.charAt(i) != '|' ; i++)
 		{
-			if (Character.isDigit(s1.charAt(i)))
+			if (Character.isDigit(s1.charAt(i))) {
+				//Prints out info about the note in string 1 (can be deleted later):
 				System.out.println("String = 1 Fret = " + s1.charAt(i) + " Note: " + Notes.bassNotes("note1",Character.getNumericValue(s1.charAt(i))));
-			
-			if (Character.isDigit(s2.charAt(i)))
+				//Coding to print out the note in string 1 in xml format:
+				System.out.println("<note>");
+				System.out.println("\t<pitch>");
+				System.out.println("\t\t<step>" +  Notes.bassNotes("note1",Character.getNumericValue(s1.charAt(i))) + "</step>");
+				System.out.println("\t\t</pitch>");
+				System.out.println("\t<notations>");
+				System.out.println("\t\t<technical>");
+				System.out.println("\t\t\t<string>1</string>");
+				System.out.println("\t\t\t<fret>" + s1.charAt(i) + "</fret>");
+				System.out.println("\t\t\t</technical>");
+				System.out.println("\t\t</notations>");
+				System.out.println("\t</note>");
+			}
+			if (Character.isDigit(s2.charAt(i))) {
+				//Prints out info about the note in string 2 (can be deleted later):
 				System.out.println("String = 2 Fret = " + s2.charAt(i) + " Note: " + Notes.bassNotes("note2",Character.getNumericValue(s2.charAt(i))));
-			
-			if (Character.isDigit(s3.charAt(i)))
+				//Coding to print out the note in string 2 in xml format:
+				System.out.println("<note>");
+				System.out.println("\t<pitch>");
+				System.out.println("\t\t<step>" + Notes.bassNotes("note2",Character.getNumericValue(s2.charAt(i))) + "</step>");
+				System.out.println("\t\t</pitch>");
+				System.out.println("\t<notations>");
+				System.out.println("\t\t<technical>");
+				System.out.println("\t\t\t<string>2</string>");
+				System.out.println("\t\t\t<fret>" + s2.charAt(i) + "</fret>");
+				System.out.println("\t\t\t</technical>");
+				System.out.println("\t\t</notations>");
+				System.out.println("\t</note>");
+			}
+			if (Character.isDigit(s3.charAt(i))) {
+				//Prints out info about the note in string 3 (can be deleted later):
 				System.out.println("String = 3 Fret = " + s3.charAt(i) + " Note: " + Notes.bassNotes("note3",Character.getNumericValue(s3.charAt(i))));
-			
-			if (Character.isDigit(s4.charAt(i)))
+				//Coding to print out the note in string 3 in xml format:
+				System.out.println("<note>");
+				System.out.println("\t<pitch>");
+				System.out.println("\t\t<step>" + Notes.bassNotes("note3",Character.getNumericValue(s3.charAt(i))) + "</step>");
+				System.out.println("\t\t</pitch>");
+				System.out.println("\t<notations>");
+				System.out.println("\t\t<technical>");
+				System.out.println("\t\t\t<string>3</string>");
+				System.out.println("\t\t\t<fret>" + s3.charAt(i) + "</fret>");
+				System.out.println("\t\t\t</technical>");
+				System.out.println("\t\t</notations>");
+				System.out.println("\t</note>");
+			}
+			if (Character.isDigit(s4.charAt(i))) {
+				//Prints out info about the note in string 4 (can be deleted later):
 				System.out.println("String = 4 Fret = " + s4.charAt(i) + " Note: " + Notes.bassNotes("note4",Character.getNumericValue(s4.charAt(i))));
-			
-			if (s5 != null && Character.isDigit(s5.charAt(i)))
+				//Coding to print out the note in string 4 in xml format:
+				System.out.println("<note>");
+				System.out.println("\t<pitch>");
+				System.out.println("\t\t<step>" + Notes.bassNotes("note4",Character.getNumericValue(s4.charAt(i))) + "</step>");
+				System.out.println("\t\t</pitch>");
+				System.out.println("\t<notations>");
+				System.out.println("\t\t<technical>");
+				System.out.println("\t\t\t<string>4</string>");
+				System.out.println("\t\t\t<fret>" + s4.charAt(i) + "</fret>");
+				System.out.println("\t\t\t</technical>");
+				System.out.println("\t\t</notations>");
+				System.out.println("\t</note>");
+			}
+			if (s5 != null && Character.isDigit(s5.charAt(i))) {
+				//Prints out info about the note in string 5 (can be deleted later):
 				System.out.println("String = 4 Fret = " + s5.charAt(i) + " Note: " + Notes.bassNotes("note5",Character.getNumericValue(s5.charAt(i))));
+				//Coding to print out the note in string 5 in xml format:
+				System.out.println("<note>");
+				System.out.println("\t<pitch>");
+				System.out.println("\t\t<step>" + Notes.bassNotes("note5",Character.getNumericValue(s5.charAt(i))) + "</step>");
+				System.out.println("\t\t</pitch>");
+				System.out.println("\t<notations>");
+				System.out.println("\t\t<technical>");
+				System.out.println("\t\t\t<string>5</string>");
+				System.out.println("\t\t\t<fret>" + s5.charAt(i) + "</fret>");
+				System.out.println("\t\t\t</technical>");
+				System.out.println("\t\t</notations>");
+				System.out.println("\t</note>");
+			}
 		}
 	}
 		
