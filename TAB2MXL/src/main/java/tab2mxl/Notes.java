7d9e72e6ba3b888257e2f3bdd8a02814ef9aa0c7
MaishaRahman98 <maisha98@my.yorku.ca>
2021-02-15 17:48:57 -0500

    update in stringinstrument

--- TAB2MXL/src/main/java/tab2mxl/Notes.java
+++ TAB2MXL/src/main/java/tab2mxl/Notes.java
@@ -1,27 +1,49 @@
 package tab2mxl;
 
 public class Notes {
-	final static String[] noteBass1 = {"G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G"};
-	final static String[] noteBass2 = {"D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D"};
-	final static String[] noteBass3 = {"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A"};
-	final static String[] noteBass4 = {"E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"};
-	final static String[] noteBass5 = {"B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"};
 	
-	public static String guitarNotes(String line, int pos) {
-		if(line == "note1") {
-			return noteBass1[pos];
-		}
-		else if(line == "note2") {
-			return noteBass2[pos];
-		}
-		else if(line == "note3") {
-			return noteBass3[pos];
-		}
-		else if(line == "note4") {
-			return noteBass4[pos];
-		}
-		else {
-			return noteBass5[pos];
+	final static String[] mainNotes = {"E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C", "Db", "D", "Eb", "E"};
+	
+	public static String bassNotes(String stringNum, int fret) {
+		if(stringNum == "String1") {
+			fret+= 3;
+			if(fret < 12) {
+			return mainNotes[fret]; 
+			}
+			else {
+			return mainNotes[fret-12];
+			
 			}
 		}
+		else if(stringNum == "String2") {
+			fret+=10;
+			if(fret < 12) {
+			return mainNotes[fret]; 
+			}
+			else {
+			return mainNotes[fret-12];
+			}
+		}
+		else if(stringNum == "String3") {
+			fret+=5;
+			if(fret < 12) {
+			return mainNotes[fret]; 
+			}
+			else {
+			return mainNotes[fret-12];
+			}
+		}
+		else if(stringNum == "String4") {
+			return mainNotes[fret]; 
+		}
+		else {
+			fret+=7;
+			if(fret < 12) {
+			return mainNotes[fret]; 
+			}
+			else {
+			return mainNotes[fret-12];
+			}
+		}
+	}
 }
--- TAB2MXL/src/main/java/tab2mxl/StringInstrument.java
+++ TAB2MXL/src/main/java/tab2mxl/StringInstrument.java
@@ -81,16 +81,24 @@ public class StringInstrument {
 		String note = "";
 		String string = "";
 		char fret = 0;
+		int cc = 1;
+		String[] allStrings = {str1, str2, str3, str4, str5, str6, str7};
+		
 		//XML declarations:
 		System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 		System.out.println("<!DOCTYPE score-partwise PUBLIC \"-//Recordare//DTD MusicXML 3.1 Partwise//EN\" \"http://www.musicxml.org/dtds/partwise.dtd\">");
 		for (int i = 2 ; str1.charAt(i) != '|' ; i++)
 		{
-			if (Character.isDigit(str1.charAt(i))) {
+			
+			for (String j: allStrings) {
+				
+			if (j != null && Character.isDigit(j.charAt(i))) {
 				//System.out.println("String = 1 Fret = " + this.getStr1().charAt(i) + " Note: " + Notes.guitarNotes("note1",Character.getNumericValue(this.getStr1().charAt(i))));
-				fret = str1.charAt(i);
-				note = Notes.guitarNotes("note1",Character.getNumericValue(str1.charAt(i)));
-				string = "1";
+				fret = j.charAt(i);
+				note = Notes.bassNotes("String" + String.valueOf(cc) ,Character.getNumericValue(j.charAt(i)));
+				//string = cc;
+				
+				//helpxml(note, string, fret);
 				
 				//Coding to print out the note in string 1 in xml format:
 				System.out.println("<note>");
@@ -99,127 +107,16 @@ public class StringInstrument {
 				System.out.println("\t\t</pitch>");
 				System.out.println("\t<notations>");
 				System.out.println("\t\t<technical>");
-				System.out.println("\t\t\t<string>" + string + "</string>");
+				System.out.println("\t\t\t<string>" + cc + "</string>");
 				System.out.println("\t\t\t<fret>" + fret + "</fret>");
 				System.out.println("\t\t\t</technical>");
 				System.out.println("\t\t</notations>");
 				System.out.println("\t</note>");
-				
-			}else if (Character.isDigit(str2.charAt(i))) {
-				//System.out.println("String = 2 Fret = " + this.getStr2().charAt(i) + " Note: " + Notes.guitarNotes("note2",Character.getNumericValue(this.getStr2().charAt(i))));
-				fret = str2.charAt(i);
-				note = Notes.guitarNotes("note2",Character.getNumericValue(str2.charAt(i)));
-				string = "2";
-				
-				//Coding to print out the note in string 2 in xml format:
-				System.out.println("<note>");
-				System.out.println("\t<pitch>");
-				System.out.println("\t\t<step>" +  note + "</step>");
-				System.out.println("\t\t</pitch>");
-				System.out.println("\t<notations>");
-				System.out.println("\t\t<technical>");
-				System.out.println("\t\t\t<string>" + string + "</string>");
-				System.out.println("\t\t\t<fret>" + fret + "</fret>");
-				System.out.println("\t\t\t</technical>");
-				System.out.println("\t\t</notations>");
-				System.out.println("\t</note>");
-				
-			}else if (Character.isDigit(str3.charAt(i))) {
-				//System.out.println("String = 3 Fret = " + this.getStr3().charAt(i) + " Note: " + Notes.guitarNotes("note3",Character.getNumericValue(this.getStr3().charAt(i))));
-				fret = str3.charAt(i);
-				note = Notes.guitarNotes("note3",Character.getNumericValue(str3.charAt(i)));
-				string = "3";
-				
-				//Coding to print out the note in string 3 in xml format:
-				System.out.println("<note>");
-				System.out.println("\t<pitch>");
-				System.out.println("\t\t<step>" +  note + "</step>");
-				System.out.println("\t\t</pitch>");
-				System.out.println("\t<notations>");
-				System.out.println("\t\t<technical>");
-				System.out.println("\t\t\t<string>" + string + "</string>");
-				System.out.println("\t\t\t<fret>" + fret + "</fret>");
-				System.out.println("\t\t\t</technical>");
-				System.out.println("\t\t</notations>");
-				System.out.println("\t</note>");
-				
-			}else if (Character.isDigit(str4.charAt(i))) {
-				//System.out.println("String = 4 Fret = " + this.getStr4().charAt(i) + " Note: " + Notes.guitarNotes("note4",Character.getNumericValue(this.getStr4().charAt(i))));
-				fret = str4.charAt(i);
-				note = Notes.guitarNotes("note4",Character.getNumericValue(str4.charAt(i)));
-				string = "4";
-				
-				//Coding to print out the note in string 4 in xml format:
-				System.out.println("<note>");
-				System.out.println("\t<pitch>");
-				System.out.println("\t\t<step>" +  note + "</step>");
-				System.out.println("\t\t</pitch>");
-				System.out.println("\t<notations>");
-				System.out.println("\t\t<technical>");
-				System.out.println("\t\t\t<string>" + string + "</string>");
-				System.out.println("\t\t\t<fret>" + fret + "</fret>");
-				System.out.println("\t\t\t</technical>");
-				System.out.println("\t\t</notations>");
-				System.out.println("\t</note>");
-				
-			}else if (str5 != null && Character.isDigit(str5.charAt(i))) {
-				//System.out.println("String = 5 Fret = " + this.getStr5().charAt(i) + " Note: " + Notes.guitarNotes("note5",Character.getNumericValue(this.getStr5().charAt(i))));
-				fret = str5.charAt(i);
-				note = Notes.guitarNotes("note5",Character.getNumericValue(str5.charAt(i)));
-				string = "5";
-				
-				//Coding to print out the note in string 5 in xml format:
-				System.out.println("<note>");
-				System.out.println("\t<pitch>");
-				System.out.println("\t\t<step>" +  note + "</step>");
-				System.out.println("\t\t</pitch>");
-				System.out.println("\t<notations>");
-				System.out.println("\t\t<technical>");
-				System.out.println("\t\t\t<string>" + string + "</string>");
-				System.out.println("\t\t\t<fret>" + fret + "</fret>");
-				System.out.println("\t\t\t</technical>");
-				System.out.println("\t\t</notations>");
-				System.out.println("\t</note>");
-				
-			}else if (str6 != null && Character.isDigit(str6.charAt(i))) {
-				//System.out.println("String = 6 Fret = " + this.getStr6().charAt(i) + " Note: " + Notes.guitarNotes("String6",Character.getNumericValue(this.getStr6().charAt(i))));
-				fret = str6.charAt(i);
-				note = Notes.guitarNotes("String6",Character.getNumericValue(str6.charAt(i)));
-				string = "6";
-				
-				//Coding to print out the note in string 6 in xml format:
-				System.out.println("<note>");
-				System.out.println("\t<pitch>");
-				System.out.println("\t\t<step>" +  note + "</step>");
-				System.out.println("\t\t</pitch>");
-				System.out.println("\t<notations>");
-				System.out.println("\t\t<technical>");
-				System.out.println("\t\t\t<string>" + string + "</string>");
-				System.out.println("\t\t\t<fret>" + fret + "</fret>");
-				System.out.println("\t\t\t</technical>");
-				System.out.println("\t\t</notations>");
-				System.out.println("\t</note>");
-				
-			}else if (str7 != null && Character.isDigit(str7.charAt(i))) {
-				//System.out.println("String = 7 Fret = " + this.getStr7().charAt(i) + " Note: " + Notes.guitarNotes("String7",Character.getNumericValue(this.getStr7().charAt(i))));
-				fret = str7.charAt(i);
-				note = Notes.guitarNotes("String7",Character.getNumericValue(str7.charAt(i)));
-				string = "7";
-				
-				//Coding to print out the note in string 7 in xml format:
-				System.out.println("<note>");
-				System.out.println("\t<pitch>");
-				System.out.println("\t\t<step>" +  note + "</step>");
-				System.out.println("\t\t</pitch>");
-				System.out.println("\t<notations>");
-				System.out.println("\t\t<technical>");
-				System.out.println("\t\t\t<string>" + string + "</string>");
-				System.out.println("\t\t\t<fret>" + fret + "</fret>");
-				System.out.println("\t\t\t</technical>");
-				System.out.println("\t\t</notations>");
-				System.out.println("\t</note>");
-				
+				cc++;
 			}
+			
+			}
+			cc = 1;
 			
 		}
 	}		
