154624a4001aa4944c063f0fe0ac7e12a9f5e342
Shaun Li <hazalw@my.yorku.ca>
2021-02-25 19:48:46 -0500

    1

--- TAB2MXL/src/main/java/tab2mxl/outputFile.java
+++ TAB2MXL/src/main/java/tab2mxl/outputFile.java
@@ -1,5 +1,36 @@
 package tab2mxl;
 
-public class outputFile {
+import java.awt.Window;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class outputFile {
+	window win;
+	String xml = "";
+	TablatureScanner ts = new TablatureScanner(xml);
+
+	public outputFile(window win) {
+		this.win = win;
+
+	}
+
+	public String createFile(String text) {
+		xml = ts.detect(text);
+		try {
+			File myObj = new File("newfile.txt");
+			FileWriter myWriter = new FileWriter("newfile.txt");
+			myWriter.write(xml);
+			myWriter.close();
+			System.out.println("file made");
+			return myObj.getName();
+
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
 }
